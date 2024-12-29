1713d716630f1a895de96514808f539683998584
AbdelrahmanNasser77 <sim.abdelrahmanmohamed36283@alexu.edu.eg>
2024-12-29 23:40:22 +0200

    remove header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import RegularShape from "./pages/RegularShape";
 import BlobShape from "./pages/BlobShape";
 
 function App() {
   return (
     <Router>
-      <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
